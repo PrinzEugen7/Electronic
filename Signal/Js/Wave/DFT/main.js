9f1a482fbb3aa269c21c8df92cfdb737bdba2457
PrinzEugen7 <nsi05162@pooae.com>
2016-08-23 22:36:36 +0900

    Update main.js

--- Signal/Js/Wave/DFT/main.js
+++ Signal/Js/Wave/DFT/main.js
@@ -1,14 +1,16 @@
 function main()
 {
-        data = new Array(0, 2, 3, 3, 2, 0, 2, 3, 3, 2, 0); 
-        alert( "計算結果　:　" + dft(data) );
+	data = new Array(0, 2, 3, 3, 2, 0, 2, 3, 3, 2, 0); 
+	alert( "計算結果　:　" + dft(data).Re ); // フーリエ変換後のデータ（実部）を表示
+	//alert( "計算結果　:　" + dft(data).Re ); // フーリエ変換後のデータ（虚部）を表示
 }
 
 function dft(a)
 {
-	var Re = [];// 実数部
-	var Im = [];// 虚数部
-	var N = a.length;
+	var Re = [];// 実部
+	var Im = [];// 虚部
+	var N = a.length; // サンプル数
+	// DFTの計算
 	for( var j=0; j<N; ++j ){
 		var re = 0.0;
 		var im = 0.0;
